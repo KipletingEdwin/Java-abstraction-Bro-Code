c2d68557dbd2cf0d075f9fb745a1161c2bcd8718
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-21 10:35:19 +0300

    implements abstraction

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //abstract =   abstract classes cannot be instantiated, but they can have a subclass
+        //             abstract methods are declared without an implementation
+
+
+
+//        Vehicle vehicle = new Vehicle();
+        Car car = new Car();
+        car.go();
+
+
+
+
+
     }
 }
